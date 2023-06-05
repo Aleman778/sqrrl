a883154109446f65acc3ecedf84bdd943866755c
Alexander Mennborg <alexander.mennborg@gmail.com>
2023-06-05 23:11:26 +0200

    Updated OpenGL example

--- examples/opengl/test.fs
+++ examples/opengl/test.fs
@@ -1,9 +1,18 @@
 #version 330 core
 out vec4 frag_color;
 
+in vec3 p;
 in vec4 vertex_color;
+
+uniform float t;
 
 void
 main() {
-    frag_color = vertex_color;
+
+    float circle1 = clamp(pow(cos(sqrt(p.x*p.x + (p.y)*(p.y))*80.0f + t), 40.0f), 0.0f, 1.0f);
+    float circle2 = clamp(pow(cos(sqrt((p.x - 1.0f)*(p.x - 1.0f) + (p.y + 1.0f)*(p.y + 1.0f))*80.0f + t), 40.0f), 0.0f, 1.0f);
+    float circle3 = clamp(pow(cos(sqrt((p.x + 1.0f)*(p.x + 1.0f) + (p.y + 1.0f)*(p.y + 1.0f))*80.0f + t), 40.0f), 0.0f, 1.0f);
+
+    frag_color = vec4(circle1, circle2, circle3, 1.0f);
+//vec4(vertex_color.x*(circle1 + circle2 + circle3), vertex_color.y*circle2, vertex_color.z*circle3, 1.0f);
 }
--- examples/opengl/test.vs
+++ examples/opengl/test.vs
@@ -2,10 +2,12 @@
 layout (location = 0) in vec3 position;
 layout (location = 1) in vec3 color;
 
+out vec3 p;
 out vec4 vertex_color;
 
 void
 main() {
+   p = position;
    gl_Position = vec4(position, 1.0f);
    vertex_color = vec4(color, 1.0f);
 }
